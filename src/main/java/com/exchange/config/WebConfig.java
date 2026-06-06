3e1aa490f27aead0d26272222e607db32a4b2fa5
fangxiong <1255404327@qq.com>
2026-06-06 17:52:35 +0800

    Add some comments.

--- src/main/java/com/exchange/config/WebConfig.java
+++ src/main/java/com/exchange/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration //配置类
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${exchange_file.local-path}")
@@ -33,6 +33,7 @@ public class WebConfig implements WebMvcConfigurer {
                 );
     }
 
+    // 配置静态资源映射
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(prefix + "**")
